aed9e6111f55016b8a30967a6f6b9d125f928e83
Johannes <fritz.johannes@gmail.com>
2019-04-30 11:37:49 +0200

    Switched Figure 1 to gta_trade_coverage

--- code/2 - The Sino-US trade war - An update/Chapter 2 script.R
+++ code/2 - The Sino-US trade war - An update/Chapter 2 script.R
@@ -34,19 +34,6 @@ source("0 report production/GTA 24/help files/GTA 24 cutoff and definitions.R")
 ### THE GTA standard colour palette
 gta_colour_palette()
 
-# trade data
-load("data/support tables/Final goods support table.Rdata")
-trade=subset(final, Year>=2008)[,c("Reporter.un","Partner.un","Year", "Tariff.line", "Value")]
-rm(final)
-names(trade)=c("i.un","a.un","year","affected.product","trade.value")
-
-
-
-
-# T1: US tariff measures (including trade defence) taken against China. And comparable measures taken against USA.
-
-# T2: US safeguard duties on steel and aluminium and retaliatory actions taken by everyone else.
-
 # F1: Stacked bar chart for annual average for 2013-16, annual 2017 and annual 2018 of Chinese exports to the USA affected by new harmful US measures introduced each year that
 ## Label 2013-16 as Obama II (2013-6). Bottom two entries in each stack should reveal extent (upper bound) of bilateral targeting to China.
 
@@ -58,7 +45,9 @@ figure2.1=data.frame(implementer=character(),
                      iii=character(),
                      iv=character()
 )
-# yr = 2019
+
+trade.coverage.estimates=data.frame()
+
 for(yr in 2013:2019){
   # prep
   f21=data.frame(implementer=c("United States of America", "China"),
@@ -67,7 +56,8 @@ for(yr in 2013:2019){
                  i=NA,
                  ii=NA,
                  iii=NA,
-                 iv=NA
+                 iv=NA,
+                 v=NA
   )
   
   
@@ -75,117 +65,133 @@ for(yr in 2013:2019){
   yr.start=paste(yr, "-01-01",sep="")
   yr.end=paste(yr, "-12-31",sep="")
   
-  trade.yr=subset(trade, year==(yr-1) & a.un %in% c(156,840) & i.un %in% c(156,840))
-  # COMMENT PB: TRADE DATA FOR 2018 NOT YET IN DATABASE, FOR 2019 I USE 2017 VALUES
-  if (yr == 2019) {trade.yr=subset(trade, year==(yr-2) & a.un %in% c(156,840) & i.un %in% c(156,840))}
-  trade.yr$year=NULL
-  names(trade.yr)=c("i.un","a.un","affected.product","trade.value")
-
+  t.data=paste(yr-1)
+  if(t.data>=2018){
+    t.data="2017"
+  }
+  
   for(i in 1:2){
     
     # (i) were tariffs or trade defence that only affected China,
-    gta_data_slicer(gta.evaluation = c("Red", "Amber"),
-                    affected.flows = "inward",
-                    implementation.period = c(yr.start, yr.end),
-                    implementing.country = as.character(f21$implementer[i]),
-                    keep.implementer = T,
-                    affected.country = as.character(f21$affected[i]),
-                    keep.affected = T,
-                    nr.affected = c(1,1),
-                    intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
-                    keep.type = T,
-                    keep.implementation.na=F
-    )
+    gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                       affected.flows = "inward",
+                       coverage.period = c(yr,yr),
+                       implementation.period = c(yr.start, yr.end),
+                       implementers = as.character(f21$implementer[i]),
+                       keep.implementer = T,
+                       exporters = as.character(f21$affected[i]),
+                       keep.exporters = T,
+                       nr.exporters = c(1,1),
+                       intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
+                       keep.type = T,
+                       trade.statistic = "value",
+                       trade.data = t.data,
+                       intra.year.duration = F)
     
-    f21.i=master.sliced
-    rm(master.sliced)
-    
-    if(nrow(f21.i)>0){
-      f21.i=cSplit(f21.i, which(names(f21.i)=="affected.product"), direction="long", sep=",")
-      f21.i=merge(f21.i, trade.yr, by=c("i.un","a.un","affected.product"), all.x=T)
-      f21.i$trade.value[is.na(f21.i$trade.value)]=0
-      
-      f21$i[i]=sum(unique(f21.i[,c("affected.product","trade.value")])$trade.value)
+    if(nrow(trade.coverage.estimates)>0){
+      f21$i[i]=trade.coverage.estimates[,4]
+    } else {
+      f21$i[i]=0
     }
+    trade.coverage.estimates=data.frame()
+    
     
     # (ii) were other harmful US policies that only affected China,
-    gta_data_slicer(gta.evaluation = c("Red", "Amber"),
-                    affected.flows = "inward",
-                    implementation.period = c(yr.start, yr.end),
-                    implementing.country = as.character(f21$implementer[i]),
-                    keep.implementer = T,
-                    affected.country = as.character(f21$affected[i]),
-                    keep.affected = T,
-                    nr.affected = c(1,1),
-                    intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
-                    keep.type = F,
-                    keep.implementation.na=F
-    )
-    f21.ii=master.sliced
-    rm(master.sliced)
     
-    if(nrow(f21.ii)>0){
-      f21.ii=cSplit(f21.ii, which(names(f21.ii)=="affected.product"), direction="long", sep=",")
-      f21.ii=merge(f21.ii, trade.yr, by=c("i.un","a.un","affected.product"), all.x=T)
-      f21.ii$trade.value[is.na(f21.ii$trade.value)]=0
-      
-      f21$ii[i]=sum(unique(f21.ii[,c("affected.product","trade.value")])$trade.value)
-      
+    gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                       affected.flows = "inward",
+                       coverage.period = c(yr,yr),
+                       implementation.period = c(yr.start, yr.end),
+                       implementers = as.character(f21$implementer[i]),
+                       keep.implementer = T,
+                       exporters = as.character(f21$affected[i]),
+                       keep.exporters = T,
+                       nr.exporters = c(1,1),
+                       intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
+                       keep.type = F,
+                       trade.statistic = "value",
+                       trade.data = t.data,
+                       intra.year.duration = F)
+    
+    if(nrow(trade.coverage.estimates)>0){
+      f21$ii[i]=trade.coverage.estimates[,4]
+    } else {
+      f21$ii[i]=0
     }
+    
+    trade.coverage.estimates=data.frame()
     
     
     # (iii) were tariffs or trade defence that affected China and other US trading partners,
-    gta_data_slicer(gta.evaluation = c("Red", "Amber"),
-                    affected.flows = "inward",
-                    implementation.period = c(yr.start, yr.end),
-                    implementing.country = as.character(f21$implementer[i]),
-                    keep.implementer = T,
-                    affected.country = as.character(f21$affected[i]),
-                    keep.affected = T,
-                    keep.others=T,
-                    intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
-                    keep.type = T,
-                    keep.implementation.na=F
-    )
-    f21.iii=master.sliced
-    f21.iii=subset(f21.iii, !intervention.id %in% f21.i$intervention.id)
-    rm(master.sliced)
+    gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                       affected.flows = "inward",
+                       coverage.period = c(yr,yr),
+                       implementation.period = c(yr.start, yr.end),
+                       implementers = as.character(f21$implementer[i]),
+                       keep.implementer = T,
+                       exporters = as.character(f21$affected[i]),
+                       keep.exporters = T,
+                       nr.exporters = c(2,999),
+                       intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
+                       keep.type = T,
+                       trade.statistic = "value",
+                       trade.data = t.data,
+                       intra.year.duration = F)
     
-    if(nrow(f21.iii)>0){
-      f21.iii=cSplit(f21.iii, which(names(f21.iii)=="affected.product"), direction="long", sep=",")
-      f21.iii=merge(f21.iii, trade.yr, by=c("i.un","a.un","affected.product"), all.x=T)
-      f21.iii$trade.value[is.na(f21.iii$trade.value)]=0
-      
-      f21$iii[i]=sum(unique(f21.iii[,c("affected.product","trade.value")])$trade.value)
+    if(nrow(trade.coverage.estimates)>0){
+      f21$iii[i]=trade.coverage.estimates[,4]
+    } else {
+      f21$iii[i]=0
     }
+    trade.coverage.estimates=data.frame()
+    
     
     # (iv) were other US policies that affected China and other US trading partners.
-    gta_data_slicer(gta.evaluation = c("Red", "Amber"),
-                    affected.flows = "inward",
-                    implementation.period = c(yr.start, yr.end),
-                    implementing.country = as.character(f21$implementer[i]),
-                    keep.implementer = T,
-                    affected.country = as.character(f21$affected[i]),
-                    keep.affected = T,
-                    keep.others=T,
-                    intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
-                    keep.type = F,
-                    keep.implementation.na=F
-    )
-    f21.iv=master.sliced
-    f21.iv=subset(f21.iv, !intervention.id %in% f21.ii$intervention.id)
-    rm(master.sliced)
+    gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                       affected.flows = "inward",
+                       coverage.period = c(yr,yr),
+                       implementation.period = c(yr.start, yr.end),
+                       implementers = as.character(f21$implementer[i]),
+                       keep.implementer = T,
+                       exporters = as.character(f21$affected[i]),
+                       keep.exporters = T,
+                       nr.exporters = c(2,999),
+                       intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
+                       keep.type = F,
+                       trade.statistic = "value",
+                       trade.data = t.data,
+                       intra.year.duration = F)
     
-    if(nrow(f21.iv)>0){
-      f21.iv=cSplit(f21.iv, which(names(f21.iv)=="affected.product"), direction="long", sep=",")
-      f21.iv=merge(f21.iv, trade.yr, by=c("i.un","a.un","affected.product"), all.x=T)
-      f21.iv$trade.value[is.na(f21.iv$trade.value)]=0
-      
-      f21$iv[i]=sum(unique(f21.iv[,c("affected.product","trade.value")])$trade.value)
-      print(i)
+    if(nrow(trade.coverage.estimates)>0){
+      f21$iv[i]=trade.coverage.estimates[,4]
+    } else {
+      f21$iv[i]=0
     }
+    trade.coverage.estimates=data.frame()
+    
+    # (v) all US policies harming China
+    gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                       affected.flows = "inward",
+                       coverage.period = c(yr,yr),
+                       implementation.period = c(yr.start, yr.end),
+                       implementers = as.character(f21$implementer[i]),
+                       keep.implementer = T,
+                       exporters = as.character(f21$affected[i]),
+                       keep.exporters = T,
+                       
+                       trade.statistic = "value",
+                       trade.data = t.data,
+                       intra.year.duration = F)
+    
+    if(nrow(trade.coverage.estimates)>0){
+      f21$v[i]=trade.coverage.estimates[,4]
+    } else {
+      f21$v[i]=0
+    }
+    trade.coverage.estimates=data.frame()
     
   }
+  
   f21[is.na(f21)]=0
   figure2.1=rbind(figure2.1, f21)
   
@@ -199,19 +205,21 @@ f1316=merge(merge(aggregate(i ~ implementer + affected, subset(figure2.1, year<=
             merge(aggregate(iii ~ implementer + affected, subset(figure2.1, year<=2016),mean),
                   aggregate(iv ~ implementer + affected, subset(figure2.1, year<=2016),mean), by=c("implementer", "affected"), all.x=T),
             by=c("implementer", "affected"), all.x=T)
+
+f1316=merge(f1316, aggregate(v ~ implementer + affected, subset(figure2.1, year<=2016),mean), 
+            by=c("implementer", "affected"), all.x=T)
+
 f1316$year="2013-16"
 
 figure2.1=rbind(f1316, subset(figure2.1, year>2016))
 
 figure2.1.xlsx=subset(figure2.1, implementer!="China")[,c(1,2,7,3:6)]
 
-figure2.1.xlsx$v <- rowSums(figure2.1.xlsx[,c("i","ii","iii","iv")], na.rm = TRUE)
-
 figure2.1.xlsx=figure2.1.xlsx[order(figure2.1.xlsx$implementer),]
 names(figure2.1.xlsx)=c("Implementer","Affected country", "Period","Targeted tariffs or trade defence", "Targeted non-tariff/trade defence",
                         "Untargeted tariffs or trade defence", "Untargeted non-tariff/trade defence","All US policies harming Chinese exports")
 
-write.xlsx(figure2.1.xlsx, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure ",chapter.number,".1 - Data for Figure ",chapter.number,".1.xlsx", sep=""), row.names=F)
+xlsx::write.xlsx(figure2.1.xlsx, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure ",chapter.number,".1 - Data for Figure ",chapter.number,".1.xlsx", sep=""), row.names=F)
 
 
 # plot here
