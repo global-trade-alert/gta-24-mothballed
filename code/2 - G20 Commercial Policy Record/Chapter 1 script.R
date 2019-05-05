8ed44195ee5b395a9ad15ec10615c4238f13179d
Johannes <fritz.johannes@gmail.com>
2019-05-05 21:40:14 +0200

    chapter 1 --> chapter 2

--- code/2 - G20 Commercial Policy Record/Chapter 1 script.R
+++ code/2 - G20 Commercial Policy Record/Chapter 1 script.R
@@ -106,7 +106,8 @@ plot.6.2.a = ggplot(plotting.data,aes(x = periods, y = total.implemented.harmful
   geom_point(colour= gta_colour$harmful[1], size=3) +
   ylab('Number of G20 implemented harmful interventions') +
   xlab('Period') + ylim(c(0, 300)) + 
-  scale_x_continuous(breaks = plotting.data$periods,labels=period.labels) + gta_theme()
+  scale_x_continuous(breaks = plotting.data$periods,labels=period.labels) + gta_theme() +
+  scale_y_continuous(sec.axis = dup_axis(), limits=c(0,300))
 
 plot.6.2.a
 
@@ -157,7 +158,8 @@ plot.6.2.b = ggplot(plotting.data,aes(x=periods, y=share.implemented.harmful.mea
   geom_point(colour=gta_colour$harmful[1], size=3) +
   xlab('Period') +  ylab('Percentage of G20 implemented measures which are harmful') + 
   scale_x_continuous(breaks = plotting.data$periods,labels=period.labels) + 
-  scale_y_continuous(breaks = seq(0,100,10),labels=paste0(seq(0,100,10),'%'), limits = (c(60,100))) + gta_theme()
+  scale_y_continuous(breaks = seq(0,100,10),labels=paste0(seq(0,100,10),'%'), limits = (c(60,100)),sec.axis = dup_axis()) + gta_theme() 
+
 
 plot.6.2.b
 
@@ -176,10 +178,15 @@ gta_plot_saver(plot=plot.6.2.b,
 g20.members=c(32, 36, 76, 124, 156, 251, 276, 699, 360, 381, 392, 484, 410, 643, 682, 710, 792, 826, 840)
 country.groups=list('G20' = g20.members)
 
+official.source.base = read_csv("data/database replica/gta_measure.csv")
+official.source.vector = subset(official.source.base, is_source_official == 1)$'title'
+
+
 rnd=1
 while(rnd<=2){
   g20.implemented.harmful.measures.policies = data.frame()
-  
+  master.sliced.titles = vector()
+  # official.source.ratio = vector()
   #get most frequent policy instruments over the 5 years
   for (year in 1:length(year.list)){
     r.period=c(year.list[[year]])
@@ -191,6 +198,8 @@ while(rnd<=2){
                     keep.implementation.na = F,
                     implementation.period = c(year.list[[year]]),
                     reporting.period = r.period)
+    master.sliced.titles = c(master.sliced.titles,as.character(master.sliced$title))
+    
     
     g20.policies = master.sliced[,colnames(master.sliced) %in% c('mast.chapter','intervention.id')]
     g20.policies = g20.policies[!duplicated(g20.policies),]
@@ -199,8 +208,18 @@ while(rnd<=2){
     
     g20.implemented.harmful.measures.policies = rbind(g20.implemented.harmful.measures.policies, g20.policies.chapters)
     
+    
+    
   }
   
+  if (rnd == 1){
+    assign('G20.official.source.ratio',(length(unique(master.sliced.titles))-length(setdiff(master.sliced.titles,official.source.vector)))/length(unique(master.sliced.titles)))
+  } 
+  
+  if (rnd == 2) {
+    assign('nonG20.official.source.ratio',(length(unique(master.sliced.titles))-length(setdiff(master.sliced.titles,official.source.vector)))/length(unique(master.sliced.titles)))
+  }
+
   top5.frequent.policies = g20.implemented.harmful.measures.policies %>% group_by(mast.chapter) %>% summarise(most.frequent = sum(n)) %>% dplyr::top_n(5)
   top5.frequent.policies = top5.frequent.policies$mast.chapter
   
@@ -278,9 +297,10 @@ while(rnd<=2){
       scale_fill_manual(name='', values = colors.1,labels=names(colors.1)) + 
       xlab('Period') + 
       gta_theme() +
-      ylab('Number of harmful policy instruments implemented by G20') + 
+      ylab('Number of harmful policy instruments\nimplemented by G20') + 
       scale_x_continuous(breaks = 1:5,labels=period.labels) +
-      theme(legend.text = element_text(size=8))
+      theme(legend.text = element_text(size=8)) +
+      scale_y_continuous(sec.axis = dup_axis())
     
     
     
@@ -299,9 +319,11 @@ while(rnd<=2){
       scale_fill_manual(name='', values = colors.2,labels=names(colors.2))+ 
       xlab('Period') + 
       gta_theme() +
-      ylab('Number of harmful policy instruments implemented by non-G20') + 
+      ylab('Number of harmful policy instruments\nimplemented by non-G20') + 
       scale_x_continuous(breaks = 1:5,labels=period.labels) +
-      theme(legend.text = element_text(size=8))
+      theme(legend.text = element_text(size=8)) +
+      scale_y_continuous(sec.axis = dup_axis())
+    
     
     plot.6.2.c
     gta_plot_saver(plot=plot.6.2.c,
@@ -318,7 +340,10 @@ while(rnd<=2){
   
 }
 
+official.source.ratio = data.frame('G20.official.source.ratio' = G20.official.source.ratio,
+                                   'nonG20.official.source.ratio' = nonG20.official.source.ratio)
 
+xlsx::write.xlsx(official.source.ratio,row.names = F, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Table 1.3 - Official source ratios.xlsx", sep=""))
 
 
 # d -----------------------------------------------------------------------
@@ -327,6 +352,7 @@ while(rnd<=2){
 # (d) the total amount of trade covered by the G20 harmful measures.
 
 value.total=data.frame()
+value.firm.specific=data.frame()
 value.per.intervention=data.frame()
 gta.evaluation=c("Red","Amber")
 
@@ -343,6 +369,23 @@ for (year in 1:length(year.list)){
   
   
   ## total
+  # gta_trade_coverage(gta.evaluation = gta.evaluation,
+  #                    implementers = 'G20',
+  #                    keep.implementer = T,
+  #                    reporting.period = r.period,
+  #                    coverage.period = c(c.period,c.period),
+  #                    implementation.period = year.list[[year]],
+  #                    trade.statistic = "value",
+  #                    trade.data = r.year, 
+  #                    intervention.ids = c(70350, 18891, 16819, 71578, 58794, 18254, 13633, 15366, 13512, 18892), #The huge Indian intervention with budget of only 87 million USD
+  #                    keep.interventions = F
+  # )
+  # 
+  # value.total=rbind(value.total,
+  #                   data.frame(period=paste(year.list[[year]], collapse=" - "),
+  #                              trade.value=trade.coverage.estimates[,4]))
+  
+  ## firm-specific
   gta_trade_coverage(gta.evaluation = gta.evaluation,
                      implementers = 'G20',
                      keep.implementer = T,
@@ -350,61 +393,136 @@ for (year in 1:length(year.list)){
                      coverage.period = c(c.period,c.period),
                      implementation.period = year.list[[year]],
                      trade.statistic = "value",
-                     trade.data = r.year
+                     trade.data = r.year, 
+                     eligible.firms = "firm-specific", keep.firms = T,
+                     intervention.ids = c(70350, 18891, 16819, 71578, 58794, 18254, 13633, 15366, 13512, 18892), #The huge Indian intervention with budget of only 87 million USD
+                     keep.interventions = F
   )
   
-  value.total=rbind(value.total,
+  value.firm.specific=rbind(value.firm.specific,
                     data.frame(period=paste(year.list[[year]], collapse=" - "),
                                trade.value=trade.coverage.estimates[,4]))
   
   
-  ## interventin-by-intervention
-  
-  gta_data_slicer(gta.evaluation = gta.evaluation,
-                  implementing.country = 'G20',
-                  keep.implementer = T,
-                  reporting.period = r.period,
-                  implementation.period = year.list[[year]],
-                  keep.implementation.na = F
-  )
-  
-  check.int=unique(master.sliced$intervention.id)
-  ms.base=master.sliced
-  
-  for(int in check.int){
-    tryCatch(gta_trade_coverage(intervention.ids = int,
-                                keep.interventions = T,
-                                coverage.period = c(c.period, c.period),
-                                trade.statistic = "value",
-                                trade.data = r.year))
-    
-    if(nrow(trade.coverage.estimates)==0){
-      
-      tv=0
-      
-    } else{
-      tv=trade.coverage.estimates[,4]
-      
-    }
-    
-    value.per.intervention=rbind(value.per.intervention,
-                                 data.frame(period=paste(year.list[[year]], collapse=" - "),
-                                            intervention.id=int,
-                                            is.firm.specific=unique(ms.base$eligible.firms[ms.base$intervention.id==int])=="firm-specific",
-                                            title=as.character(unique(subset(ms.base, intervention.id==int)$title)),
-                                            trade.value=tv))
-    
-    trade.coverage.estimates=data.frame()
-    
-    print(int)
-    
-  }
-  
-  rm(check.int, master.sliced)
+  ## intervention-by-intervention
+  # 
+  # gta_data_slicer(gta.evaluation = gta.evaluation,
+  #                 implementing.country = 'G20',
+  #                 keep.implementer = T,
+  #                 reporting.period = r.period,
+  #                 implementation.period = year.list[[year]],
+  #                 keep.implementation.na = F, 
+  #                 intervention.ids = c(70350), #The huge Indian intervention with budget of only 87 million USD
+  #                 keep.interventions = F
+  # )
+  # 
+  # check.int=unique(master.sliced$intervention.id)
+  # ms.base=master.sliced
+  # 
+  # for(int in check.int){
+  #   tryCatch(gta_trade_coverage(intervention.ids = int,
+  #                               keep.interventions = T,
+  #                               coverage.period = c(c.period, c.period),
+  #                               trade.statistic = "value",
+  #                               trade.data = r.year))
+  #   
+  #   if(nrow(trade.coverage.estimates)==0){
+  #     
+  #     tv=0
+  #     
+  #   } else{
+  #     tv=trade.coverage.estimates[,4]
+  #     
+  #   }
+  #   
+  #   value.per.intervention=rbind(value.per.intervention,
+  #                                data.frame(period=paste(year.list[[year]], collapse=" - "),
+  #                                           intervention.id=int,
+  #                                           is.firm.specific=unique(ms.base$eligible.firms[ms.base$intervention.id==int])=="firm-specific",
+  #                                           title=as.character(unique(subset(ms.base, intervention.id==int)$title)),
+  #                                           trade.value=tv))
+  #   
+  #   trade.coverage.estimates=data.frame()
+  #   
+  #   print(int)
+  #   
+  # }
+  # 
+  # rm(check.int, master.sliced)
   
   print(paste(year.list[[year]], collapse=" - "))
 }
 
+#### Piotr's code
 xlsx::write.xlsx(value.total, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure 1.4 - Trade value totals.xlsx", sep=""))
-xlsx::write.xlsx(value.per.intervention, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure 1.4 - Trade value per intervention.xlsx", sep=""))
 
+value.total$period.1 = 1:5
+
+fig4 = ggplot(data=value.total, aes(x=period.1,y=trade.value)) + geom_col(fill=gta_colour$blue[1]) +
+  xlab('Period') + gta_theme() +
+  ylab('Value of trade billions USD harmed\nby G20 harmful measures') + 
+  scale_x_continuous(breaks = 1:5,labels=period.labels) +
+  scale_y_continuous(breaks = seq(0,3e12, 5e11), labels=paste(seq(0,3000,500),'bln'), sec.axis = dup_axis()) 
+fig4
+
+names(value.firm.specific) <- c("period", "firm.specific")
+value.total <- merge(value.total, value.firm.specific, by="period", all = T)
+
+# fig4.firm.spec = ggplot(data=value.total.long, aes(x=period.1,y=trade.value,fill=firm.spec.status)) + geom_col() +
+#   xlab('Period') + gta_theme() +
+#   ylab('Value of trade in billions USD\nharmed by G20 harmful measures') + 
+#   scale_x_continuous(breaks = 1:5,labels=period.labels) +
+#   scale_y_continuous(breaks = seq(0,3e12, 5e11), labels=paste(seq(0,3000,500),'bln'), sec.axis = dup_axis()) + 
+#   scale_fill_manual(name='',values=gta_colour$blue[c(4,1)],labels=c('Firm specific','Non-firm specific'))
+# fig4.firm.spec
+
+gta_plot_saver(fig4,
+               path=paste("0 report production/GTA 24/tables & figures/",output.path, sep=""),
+               name="Figure 1.4 - Value of trade harmed by G20 harmful measures")
+
+# gta_plot_saver(fig4.firm.spec,
+#                path=paste("0 report production/GTA 24/tables & figures/",output.path, sep=""),
+#                name="Figure 1.4 - firm-specific Value of trade harmed by G20 harmful measures")
+
+
+
+
+
+#### Previous code:
+# 
+# # value.total is just taking the first values of each period, code does not work
+# xlsx::write.xlsx(value.total, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure 1.4 - Trade value totals.xlsx", sep=""))
+# xlsx::write.xlsx(value.per.intervention, file=paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure 1.4 - Trade value per intervention.xlsx", sep=""))
+# 
+# #value.total = xlsx::read.xlsx(paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure 1.4 - Trade value totals.xlsx", sep=""),sheetName = 'Sheet1')
+# value.per.intervention = xlsx::read.xlsx(paste("0 report production/GTA 24/tables & figures/",output.path,"/Figure 1.4 - Trade value per intervention.xlsx", sep=""),sheetName = 'Sheet1')
+# 
+# value.total = aggregate(trade.value~period,value.per.intervention,sum)
+# value.total$period.1 = 1:5
+# value.total$firm.spec = aggregate(trade.value~period,subset(value.per.intervention, is.firm.specific == T),sum)$trade.value
+# value.total$non.firm.spec = value.total$trade.value-value.total$firm.spec
+# 
+# value.total.long = gather(value.total[,c('period.1','firm.spec','non.firm.spec')], firm.spec.status, trade.value,firm.spec:non.firm.spec, factor_key=TRUE)
+# 
+# fig4 = ggplot(data=value.total, aes(x=period.1,y=trade.value)) + geom_col(fill=gta_colour$blue[1]) +
+#   xlab('Period') + gta_theme() +
+#   ylab('Value of trade billions USD\nharmed by G20 harmful measures') + 
+#   scale_x_continuous(breaks = 1:5,labels=period.labels) +
+#   scale_y_continuous(breaks = seq(0,3e12, 5e11), labels=paste(seq(0,3000,500),'bln'), sec.axis = dup_axis()) 
+# fig4
+# 
+# fig4.firm.spec = ggplot(data=value.total.long, aes(x=period.1,y=trade.value,fill=firm.spec.status)) + geom_col() +
+#   xlab('Period') + gta_theme() +
+#   ylab('Value of trade in billions USD\nharmed by G20 harmful measures') + 
+#   scale_x_continuous(breaks = 1:5,labels=period.labels) +
+#   scale_y_continuous(breaks = seq(0,3e12, 5e11), labels=paste(seq(0,3000,500),'bln'), sec.axis = dup_axis()) + 
+#   scale_fill_manual(name='',values=gta_colour$blue[c(4,1)],labels=c('Firm specific','Non-firm specific'))
+# fig4.firm.spec
+# 
+# gta_plot_saver(fig4,
+#                path=paste("0 report production/GTA 24/tables & figures/",output.path, sep=""),
+#                name="Figure 1.4 - Value of trade harmed by G20 harmful measures")
+# 
+# gta_plot_saver(fig4.firm.spec,
+#                path=paste("0 report production/GTA 24/tables & figures/",output.path, sep=""),
+#                name="Figure 1.4 - firm-specific Value of trade harmed by G20 harmful measures")
