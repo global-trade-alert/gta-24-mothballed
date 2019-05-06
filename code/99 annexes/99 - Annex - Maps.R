9a2032034b174ed87dc44605e4962cdbad28e310
Johannes <fritz.johannes@gmail.com>
2019-05-06 13:25:17 +0200

    changed categories, marked greenland grey

--- code/99 annexes/99 - Annex - Maps.R
+++ code/99 annexes/99 - Annex - Maps.R
@@ -18,11 +18,11 @@ loadfonts(device="cairo_ps")
 loadfonts(device="win")
 
 # setup
-setwd("GTA cloud/0 report production/GTA 24")
 # setwd("/Users/patrickbuess/Dropbox/Collaborations/GTA cloud/0 report production/GTA 24/")
-load("../../data/master_plus.Rdata")
+
+load("data/master_plus.Rdata")
 gtalibrary::gta_colour_palette()
-source("help files/GTA 24 cutoff and definitions.R")
+source("0 report production/GTA 24/help files/GTA 24 cutoff and definitions.R")
 g20.members=c("32", "36", "76", "124", "156", "251", "276", "699", "360", "381", "392", "484", "410", "643", "682", "710", "792", "826", "840")
 
 op <- par(family = "HersheyGothicEnglish")
@@ -45,7 +45,7 @@ par(op)
 
 
 
-conversion <- read.csv("../../R help files/country_iso_un.csv", sep=";")
+conversion <- read.csv("R help files/country_iso_un.csv", sep=";")
 setnames(conversion, old="UN", new="i.un")
 
 
@@ -84,11 +84,12 @@ marked.country <- gta_un_code_vector(cty)
 map1 = ggplot() +
   geom_polygon(data= subset(world, country != "Antarctica"), aes(x = long, y = lat, group = group, fill = value), size = 0.15, color = "white") +
   geom_polygon(data=subset(world, UN == marked.country), aes(x=long, y=lat, group = group), fill=gta_colour$turquoise[4], size = 0.15, colour = "white") +
+  geom_polygon(data=subset(world, country == "Greenland"), aes(x=long, y=lat, group = group), fill="#dadada", size = 0.15, colour = "white") +
   coord_fixed() + # Important to fix world map proportions
   scale_y_continuous(limits=c(-55,85))+
   scale_x_continuous(limits=c(-169,191))+
   labs(x="", y="") +
-  scale_fill_gradientn(colours = c(gta_colour$blue.shades(4),"#dadada"), values=c(1,0.21,0.11,0.01,0), breaks=c(0, seq(20,60,20), max(data$value)), position="bottom", labels=c("0","1-19","20-39","40-59","60 or more")) + # Set color gradient
+  scale_fill_gradientn(colours = c(gta_colour$blue.shades(4),"#dadada"), values=c(1,0.21,0.11,0.01,0), breaks=c(0,50,100,200, max(data$value)), position="bottom", labels=c("0","1-50","51-101","101-200","201 or more")) + # Set color gradient
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank(),
@@ -109,7 +110,7 @@ map1 = ggplot() +
 map1
 
 gta_plot_saver(plot=map1,
-               path="tables & figures/annex - p. 2 - maps/",
+               path="0 report production/GTA 24/tables & figures/annex - p. 2 - maps/",
                name=paste0("map_",unique(conversion$name[conversion$i.un==cty]),"_top"),
                width = 21,
                height = 12)
@@ -140,11 +141,12 @@ marked.country <- gta_un_code_vector(cty)
 map2 = ggplot() +
   geom_polygon(data= subset(world, country != "Antarctica"), aes(x = long, y = lat, group = group, fill = value), size = 0.15, color = "white") +
   geom_polygon(data=subset(world, UN == marked.country), aes(x=long, y=lat, group = group), fill=gta_colour$turquoise[4], size = 0.15, colour = "white") +
+  geom_polygon(data=subset(world, country == "Greenland"), aes(x=long, y=lat, group = group), fill="#dadada", size = 0.15, colour = "white") +
   coord_fixed() + # Important to fix world map proportions
   labs(x="", y="") +
   scale_y_continuous(limits=c(-55,85))+
   scale_x_continuous(limits=c(-169,191))+
-  scale_fill_gradientn(colours = c(gta_colour$blue.shades(4),"#dadada"), values=c(1,0.21,0.11,0.01,0), breaks=c(0, seq(20,60,20), max(data$value)), position="bottom", labels=c("0","1-19","20-39","40-59","60 or more")) + # Set color gradient
+  scale_fill_gradientn(colours = c(gta_colour$blue.shades(4),"#dadada"), values=c(1,0.21,0.11,0.01,0), breaks=c(0,50,100,200, max(data$value)), position="bottom", labels=c("0","1-50","51-101","101-200","201 or more")) + # Set color gradient
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank(),
@@ -166,7 +168,7 @@ map2 = ggplot() +
 map2
 
 gta_plot_saver(plot=map2,
-               path="tables & figures/annex - p. 2 - maps/",
+               path="0 report production/GTA 24/tables & figures/annex - p. 2 - maps/",
                name=paste0("map_",unique(conversion$name[conversion$i.un==cty]),"_bottom"),
                width = 21,
                height = 12)
